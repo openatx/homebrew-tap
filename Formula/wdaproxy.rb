92231cba248e66a5fd08c8146782aea336035a61
goreleaserbot <goreleaser@carlosbecker.com>
2017-08-22 09:47:10 +0800

    wdaproxy version 0.1.1

--- Formula/wdaproxy.rb
+++ Formula/wdaproxy.rb
@@ -1,13 +1,17 @@
 class Wdaproxy < Formula
   desc ""
   homepage ""
-  url "https://github.com/openatx/wdaproxy/releases/download/0.1.0/wdaproxy_0.1.0_darwin_amd64.tar.gz"
-  version "0.1.0"
-  sha256 "5313d566a29dad8b19f8844d113cdb027729169a1e26c1ac6f4824dd120985c9"
-  
+  url "https://github.com/openatx/wdaproxy/releases/download/0.1.1/wdaproxy_0.1.1_darwin_amd64.tar.gz"
+  version "0.1.1"
+  sha256 "6eea9b5fc9d73c7961ab4440cefcbede4bd193daa65b40bd0290de3389fc578e"
+
   depends_on "libimobiledevice"
 
   def install
     bin.install "wdaproxy"
+  end
+
+  test do
+    
   end
 end
