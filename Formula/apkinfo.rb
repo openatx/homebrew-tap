1962fef50afd4d171fd4cc5368e4c8e356e64c7d
goreleaserbot <goreleaser@carlosbecker.com>
2018-07-23 21:38:35 +0800

    Brew formula update for apkinfo version 0.3

--- Formula/apkinfo.rb
+++ Formula/apkinfo.rb
@@ -1,9 +1,9 @@
 class Apkinfo < Formula
   desc ""
   homepage ""
-  url "https://github.com/codeskyblue/apkinfo/releases/download/0.2/apkinfo_0.2_darwin_amd64.tar.gz"
-  version "0.2"
-  sha256 "159b3b4dc9cee9a1efaefd05f7315f0ee9b2b0d87509a0a99b55733330162ddf"
+  url "https://github.com/codeskyblue/apkinfo/releases/download/0.3/apkinfo_0.3_darwin_amd64.tar.gz"
+  version "0.3"
+  sha256 "9517d32ade714ab0cb62e98dcdce059fc081c2065527fcf64a4c3a6cf2e4ab1b"
 
   def install
     bin.install "apkinfo"
